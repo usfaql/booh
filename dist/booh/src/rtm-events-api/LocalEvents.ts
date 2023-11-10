ec180f11b262f783b65cd2f97e9809ebb1c75957
usfaql <146733467+usfaql@users.noreply.github.com>
2023-11-10 14:52:28 +0300

    Delete dist/booh directory

--- dist/booh/src/rtm-events-api/LocalEvents.ts
+++
@@ -1,6 +0,0 @@
-import {EventEmitter} from 'events';
-export enum LocalEventsEnum {
-  ACTIVE_SPEAKER = 'ACTIVE_SPEAKER',
-}
-const LocalEventEmitter = new EventEmitter();
-export default LocalEventEmitter;
